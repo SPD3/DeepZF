8e875c8c6ec91ea1d7967c981b7031e4544906b2
SPD3 <sean@ayerdoyles.org>
2023-09-15 14:05:01 -0400

    Got rid of download validation

--- load_pretrained_proteinbert_model.py
+++ load_pretrained_proteinbert_model.py
@@ -3,4 +3,4 @@ import os
 
 current_working_directory = os.getcwd()
 
-load_pretrained_model(current_working_directory + "/BindZF_predictor/code")
+load_pretrained_model(current_working_directory + "/BindZF_predictor/code", validate_downloading=False)
